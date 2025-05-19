b2078b435c36c3dd97173ae77cd2cc34a8c52778
jesteban97 <jesteban.aristizabal@udea.edu.co>
2025-05-18 21:21:26 -0500

    fix Id error

--- src/app/drivers/[id]/page.tsx
+++ src/app/drivers/[id]/page.tsx
@@ -1,1 +1,12 @@
 
+// src/app/drivers/[id]/page.tsx
+
+import React from 'react';
+
+export default function DriverPage({ params }: { params: { id: string } }) {
+  return (
+    <div>
+      <h1>Driver ID: {params.id}</h1>
+    </div>
+  );
+}
