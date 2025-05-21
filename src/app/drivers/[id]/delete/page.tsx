eb9e24df2ed2fae0901f5b7b2ce7c689dec5ebed
jesteban97 <jesteban.aristizabal@udea.edu.co>
2025-05-21 00:29:04 -0500

    ajustes de variables no usadas

--- src/app/drivers/[id]/delete/page.tsx
+++ src/app/drivers/[id]/delete/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 export default function EliminarConductor() {
-  const [foto, setFoto] = useState('/images/foto-conductor.jpg'); // Ruta en public/
+  const [foto] = useState('/images/foto-conductor.jpg'); // Ruta en public/
 
 
   return (
